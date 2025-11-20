bc8f6b4d5e90f03677ad858da50f09ffde1d573a
Marcellina II (She/Her) <sibyl@carpocratian.org>
2025-11-20 00:25:42 -0500

    Why not a Nostr example?

--- sdk/js/debug-crypto.ts
+++
@@ -1,8 +0,0 @@
-// sdk/js/debug-crypto.ts
-import { p256 } from '@noble/curves/p256';
-
-console.log('--- Debugging p256 export ---');
-console.log('Type:', typeof p256);
-console.log('Keys:', Object.keys(p256));
-console.log('Has hashToCurve?', typeof p256.hashToCurve);
-console.log('Has ProjectivePoint?', !!p256.ProjectivePoint);
